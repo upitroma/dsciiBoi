dac19da011768cfb5c06102ec69d658ef9dae774
upitroma <allen@rudolphweb.com>
2020-09-25 16:40:40 -0400

    made a disk

--- script.js
+++ script.js
@@ -5,11 +5,27 @@ var ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+//load assets
+var blueGlowingRing = new Image();
+blueGlowingRing.src = "assets/blueGlowingRing.png"
+
+var disc = {
+    x: 50,
+    y: 50,
+    radius: 50,
+    angle: .1, //radians
+    speed: 10
+}
 
 
 function drawBackground(){
     ctx.fillStyle = "black"
     ctx.fillRect(0, 0, canvas.width, canvas.height)
+}
+
+function drawDisc(){
+    r=disc.radius
+    ctx.drawImage(blueGlowingRing, disc.x-r, disc.y-r, disc.radius*2, disc.radius*2);
 }
 
 
@@ -23,6 +39,7 @@ function update(deltatime){
     canvas.height = window.innerHeight;
 
     drawBackground()
+    drawDisc()
 }
   
 //tick
