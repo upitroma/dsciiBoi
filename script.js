fccc93837ca62b793c4966f7821dc050c94895a6
upitroma <allen@rudolphweb.com>
2020-10-29 19:35:16 -0400

    everything is broken, but it's better

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ class Wall{
         this.color=0;
     }
 }
-var walls=[new Wall(1000,450,400,100,2*Math.PI/8)]
+var walls=[new Wall(800,200,300,200,12*Math.PI/8)]
 
 //resize the canvas when the window is resized
 window.addEventListener("resize", resizeWindow);
@@ -169,25 +169,53 @@ function checkDiscCollision(){
                 discRelativeAngle=Math.atan2(rotatedY-w.centerY,rotatedX-w.centerX)
                 console.log(discRelativeAngle)
 
+                //testing
+                discVelocityMagnitude=Math.sqrt((d.xVelo*d.xVelo)+(d.yVelo*d.yVelo))//pythagorean
+                //console.log(discVelocityMagnitude)
+                discVelocityAngle=Math.atan2(d.yVelo,d.xVelo)
+                discVelocityAngle-=w.angle
+                //console.log(discVelocityAngle)
+                //console.log((Math.cos(discVelocityAngle)*discVelocityMagnitude))
+
                 regionAngle=Math.atan2(w.height/2,w.width/2)
+
+                n=0
+                newDiscVelocityAngle=0
                
                 //figure out which side it's on
                 if(discRelativeAngle<regionAngle&&discRelativeAngle>-regionAngle){
                     console.log("right")
+                    n=Math.PI
                 }
                 else if(discRelativeAngle<Math.PI-regionAngle&&discRelativeAngle>regionAngle){
                     console.log("bottom")
+                    n=Math.PI/2
                 }
                 else if((discRelativeAngle<Math.PI&&discRelativeAngle>Math.PI-regionAngle)||(discRelativeAngle>-Math.PI&&discRelativeAngle<regionAngle-Math.PI)){
                     console.log("left")
+                    n=0
                 }
                 else if(discRelativeAngle<-regionAngle&&discRelativeAngle>regionAngle-Math.PI){
                     console.log("top")
+                    n=3*Math.PI/2
                 }
                 else{
                     console.log("corner or center or error")
+                    n=0
                 }
 
+                
+
+                newDiscVelocityAngle=((2*n)-discVelocityAngle)
+
+                newDiscVelocityAngle+=w.angle
+
+                d.xVelo=(Math.cos(newDiscVelocityAngle)*discVelocityMagnitude)
+                d.yVelo=(Math.sin(newDiscVelocityAngle)*discVelocityMagnitude)
+
+                d.x+=d.xVelo*.02//.01 should be last frame's deltatime
+                d.y+=d.yVelo*.02
+                
 
             }
             else{
@@ -219,7 +247,7 @@ function update(deltatime){
     checkBounceDecay()
     moveDisc(deltatime)
 
-    //walls[0].angle+=deltatime*5
+    walls[0].angle+=deltatime*.2
 
     //graphic layers
     drawBackground()
