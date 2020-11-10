d23c2f3b5f8df93167cf40b9250627e5b32f839d
upitroma <allen@rudolphweb.com>
2020-11-10 12:58:41 -0500

    added new level

--- assets/levels/level1.js
+++ assets/levels/level1.js
@@ -1,11 +1,19 @@
-const player={"x":400,"y":400}
-const walls=[
-    new Wall(1200,300,300,200, 0),
-    new Wall(1200,600,300,200, 0),
-    new Wall(200,450,100,400, 0)
+var player = {
+    x: 250,
+    y: 400,
+}
+
+var walls=[
+    new Wall(700,700,300,100, 0),
+    new Wall(1000,850,300,50, 0),
+    new Wall(100,825,75,75, 0),
+    new Wall(1000,300,400,200, 0),
+    new Wall(1350,350,500,100, 0),
 ]
+
 var enemies=[
-    new Enemy(100,450,100,100,0),
-    new Enemy(400,450,100,100,0),
-    new Enemy(100,600,100,100,0),
+    new Enemy(1000,800,50,50,0),
+    new Enemy(100,750,50,50,0),
+    new Enemy(1500,250,50,50,0),
+    new Enemy(1300,250,50,50,0),
 ]
