edb0449bb8fb681b07d8c1b221240c56e74088f1
BaileyPaulK <bapk626@gmail.com>
2020-11-12 13:54:47 -0500

    Lots of shit

--- script.js
+++ script.js
@@ -12,6 +12,13 @@ var blueGlowingRing = new Image();
 blueGlowingRing.src = "assets/blueGlowingRing.png"
 var orangeGlowingRing = new Image();
 orangeGlowingRing.src = "assets/orangeGlowingRing.png"
+var mineArm = new Image();
+mineArm.src = "assets/arm.png";
+var mouse = {
+    'x': 0,
+    'y': 0,
+    'd': 0
+}
 
 class Disc{
     constructor(x=50,y=50,xVelo=500,yVelo=600, discId=0, bounceDecay=3){
@@ -46,14 +53,20 @@ function resizeWindow(){
 }
 
 //launch disc on mouse click
-canvas.addEventListener("mousedown", launchDisc, false)
-function launchDisc(event){
-    discs.push(new Disc(
-        gameWidth/2,
-        gameHeight/2,
-        ((event.x-((window.innerWidth-canvas.width)/2))-(canvas.width/2)) / scale,
-        ((event.y-((window.innerHeight-canvas.height)/2))-(canvas.height/2)) / scale)
-        )
+canvas.addEventListener("mousedown", setmousedown, false)
+function setmousedown(event){
+        mouse.d = 1;
+        armyBoi.setThrow();  
+}
+canvas.addEventListener("mouseup", setmouseup, false)
+function setmouseup(event){
+        mouse.d = 0; 
+}
+
+canvas.addEventListener("mousemove", mouseUpdate, false)
+function mouseUpdate(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
 }
 
 function checkBounceDecay(){
@@ -65,6 +78,77 @@ function checkBounceDecay(){
         }
     }
 }
+
+class Arm{
+    constructor() {
+        this.status = 0;
+        this.theta = 0;
+        this.x = gameWidth /2;
+        this.y = gameHeight /2;
+        this.width = 393 / 10;
+        this.height = 1200 / 10;
+        this.throw = 0;
+    }
+    update(deltatime) {
+        //console.log(this.theta);
+        if (this.status == 0) {
+            this.theta = Math.atan2((mouse.y-window.innerHeight/2), (mouse.x-window.innerWidth/2));
+        } else if (this.status == 1){
+            this.theta += this.speed * 5 * deltatime;
+            if (mouse.d)
+            {
+                this.speed += 1 * deltatime;
+                if (this.theta > Math.PI)
+                {
+                    this.theta -= Math.PI * 2;
+                }
+            }
+            else if (this.theta >= this.throw + Math.PI * 2) {
+                this.status = 2;
+
+                discs.push(new Disc(
+                    this.x  + Math.cos(this.throw) * this.height,
+                    this.y + Math.sin(this.throw) * this.height,
+                    500 * this.speed * Math.cos(this.throw),
+                    500 * this.speed * Math.sin(this.throw))
+                    )  
+            }
+        } else {
+            this.theta += 15 * deltatime;
+            if (this.theta > Math.PI ){
+                this.theta -= Math.PI * 2;
+            }
+            this.throw = Math.atan2((mouse.y-window.innerHeight/2), (mouse.x-window.innerWidth/2));
+            var diff = Math.abs((this.throw + Math.PI) - (this.theta + Math.PI));
+            if (diff <= Math.PI * 0.1) {
+                this.status = 0;
+            }
+        }
+        this.render();
+    }
+    render() {
+        ctx.save();
+        ctx.translate(this.x * scale, this.y * scale);
+        ctx.rotate(this.theta - Math.PI / 2);
+        ctx.drawImage(mineArm, -1*(this.width * scale) / 2, 0, this.width * scale, this.height * scale);
+        if (this.status == 1) {
+            ctx.drawImage(blueGlowingRing, -1*(this.width * scale) / 2, (this.height - 30) * scale, 80*scale, 80*scale)
+        }
+        ctx.restore();
+    }
+    setThrow() {
+        if (this.status == 0) {
+            this.status = 1;
+            this.speed = 0;
+            this.throw = this.theta;
+        }
+    }
+    setPos(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+let armyBoi = new Arm;
 
 function drawBackground(){
     ctx.fillStyle = "black"
@@ -89,6 +173,7 @@ function drawPlayer(){
     ctx.fillStyle = "blue"
     w= 80 * scale
     ctx.fillRect((canvas.width/2)-(w/2), (canvas.height/2)-(w/2), w, w);
+    //armyBoi.setPos(x, y);     //if we change position, pass to arm pls :)
 }
 
 function moveDisc(deltatime){
@@ -132,6 +217,7 @@ function update(deltatime){
     moveDisc(deltatime)
     drawBackground()
     drawPlayer()//if we decide to make player static, it could just become a part of the background
+    armyBoi.update(deltatime);
     drawDisc()
 }
   
